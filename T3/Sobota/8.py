ab7f6bb91191740c65667c5ea51502aaa93eb30c
Microsoft Learn Student <learn@contoso.com>
2022-12-10 11:35:58 +0100

    8.py

--- T3/Sobota/8.py
+++ T3/Sobota/8.py
@@ -1,14 +1,31 @@
-lista1 = [1, 2, 3, [100, 200], (10, 20, 30)]
-lista2 = list(lista1)
+import copy
 
-lista1.append(40)
-lista1[3].remove(200)
+class Pociag:
+    def __init__(self, pasazer=None):
+        if pasazer is None:
+            self.pasazer = []
+        else:
+            self.pasazer = list(pasazer)
+    
+    def odbieranie(self, name):
+        self.pasazer.append(name)
 
-print("lista1: ",lista1)
-print("lista2: ",lista2)
+    def wysylanie(self, name):
+        self.pasazer.remove(name)
 
-lista2[3] += [300,400]
-lista2[-1] += (50,)
+pociag1 = Pociag(["Anna","Bartlomiej","Damian","Daniel"])
+pociag2 = copy.copy(pociag1)
+pociag3 = copy.deepcopy(pociag1)
 
-print("lista1: ",lista1)
-print("lista2: ",lista2)
+print(id(pociag1))
+print(id(pociag2))
+print(id(pociag3))
+
+pociag1.wysylanie("Damian")
+print(pociag2.pasazer)
+
+print(id(pociag1.pasazer))
+print(id(pociag2.pasazer))
+print(id(pociag3.pasazer))
+
+print(pociag3.pasazer)
