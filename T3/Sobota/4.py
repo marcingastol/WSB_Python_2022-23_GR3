902ca113401453569b98909770c57440da2e7a93
Microsoft Learn Student <learn@contoso.com>
2022-12-10 09:39:34 +0100

    4.py

---
+++ T3/Sobota/4.py
@@ -0,0 +1,16 @@
+x = None
+print(x is None)
+
+####################################
+
+koniec = object()
+
+def abc():
+    node = None
+    if node is koniec:
+        return
+
+print(abc)
+
+a == b
+a.__eq__(b)
