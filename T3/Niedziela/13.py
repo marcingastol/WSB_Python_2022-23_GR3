74280b118dff2db27bf274f34c45f36fc0d5a186
Microsoft Learn Student <learn@contoso.com>
2022-12-11 13:11:38 +0100

    13.py

--- T3/Niedziela/13.py
+++ T3/Niedziela/13.py
@@ -2,21 +2,18 @@ class Pies:
     def __init__(self, nazwaPsa):
         self.nazwa_ukryta = nazwaPsa
 
-    def getter(self):
+    @property
+    def nazwa(self):
         print("Getter")
         return self.nazwa_ukryta
 
-    def setter(self, nazwaPsa):
+    @nazwa.setter
+    def nazwa(self, nazwaPsa):
         print("Setter")
         self.nazwa_ukryta = nazwaPsa
 
-    nazwa = property(getter, setter)
-
 
 pies1 = Pies("Puszek")
-
-#pies1.setter("Puszka")
-#print(pies1.getter())
 
 pies1.nazwa = "Puszka"
 print(pies1.nazwa)
