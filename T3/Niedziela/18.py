9ceb3a143314f163f3b6a387af9d1129c1f6c9d6
Microsoft Learn Student <learn@contoso.com>
2022-12-11 14:39:41 +0100

    18.py

--- T3/Niedziela/18.py
+++ T3/Niedziela/18.py
@@ -1,8 +1,19 @@
-class Klasa:
+class Tekst():
+    def __init__(self, slowo1):
+        self.slowo1 = slowo1
 
-    @staticmethod
-    def obiekty():
-        print("Jestem tutaj!")
+    def __eq__(self, slowo2):
+        return self.slowo1.lower() == slowo2.slowo1.lower()
+
+    def __repr__(self):
+        return '||| ' + self.slowo1 + ' |||'
+
+    def __str__(self):
+        return self.slowo1
 
 
-Klasa.obiekty()
+a = Tekst("abc") ##########
+b = Tekst("ABC")
+c = Tekst("qwerty")
+
+print(a.__repr__())
